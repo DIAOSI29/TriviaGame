982bc060564a9dbc373476c6ca50c2675baca6d7
TianQin <qintian29@gmail.com>
2019-10-19 15:47:01 +0800

    added all question details including queries, answers, answer index and congratulations image address

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,41 @@
-let levels = [{}];
+let levels = [
+  {
+    question: "How many legs do butterflies have?",
+    possibleAnswers: [0, 2, 4, 6],
+    answerIndex: 3,
+    congratImageUrl:
+      "https://media.giphy.com/media/oX9prChocapB0V9U4X/giphy.gif"
+  },
+  {
+    question: "Which team won 2014 FIFA World Cup in Brazil?",
+    possibleAnswers: ["Argentina", "Brazil", "Germany", "Netherlands"],
+    answerIndex: 2,
+    congratImageUrl: "https://media.giphy.com/media/T9fHnACJJq8Ba/giphy.gif"
+  },
+  {
+    question: "Who directed the 2015 movie &quot;The Revenant&quot;?",
+    possibleAnswers: [
+      "Alejandro G. I&ntilde;&aacute;rritu",
+      "Christopher Nolan",
+      "David Fincher",
+      "Wes Anderson"
+    ],
+    answerIndex: 0,
+    congratImageUrl:
+      "https://pmcvariety.files.wordpress.com/2019/02/alejandro-g.-inarritu.jpg?w=1000"
+  },
+  {
+    question: "What was the first feature-length computer-animated movie?",
+    possibleAnswers: ["Tron", "Toy Story", "Lion king", "101 Dalmatians"],
+    answerIndex: 1,
+    congratImageUrl:
+      "https://media.giphy.com/media/wcFlXfhyyyp0c/giphy.gifww.baidu.com"
+  },
+  {
+    question:
+      "Which of the following car manufacturers had a war named after it?",
+    possibleAnswers: ["Toyota", "Honda", "Greatwall", "Volkswagen"],
+    answerIndex: 0,
+    congratImageUrl: "https://media.giphy.com/media/NdpQJAQuBFQJ2/giphy.gif"
+  }
+];

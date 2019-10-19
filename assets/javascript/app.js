5711e9896de81c760b96536e7573c3d252f58744
TianQin <qintian29@gmail.com>
2019-10-19 16:24:44 +0800

    added back answerIndex for future use while still maintaining current answer key, started to declare function for user guess check

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,15 @@ let levels = [
   {
     question: "How many legs do butterflies have?",
     possibleAnswers: [0, 2, 4, 6],
-    answer: 3,
+    answer: 6,
+    answerIndex: 3,
     congratImageUrl:
       "https://media.giphy.com/media/oX9prChocapB0V9U4X/giphy.gif"
   },
   {
     question: "Which team won 2014 FIFA World Cup in Brazil?",
     possibleAnswers: ["Argentina", "Brazil", "Germany", "Netherlands"],
+    answer: "Germany",
     answerIndex: 2,
     congratImageUrl: "https://media.giphy.com/media/T9fHnACJJq8Ba/giphy.gif"
   },
@@ -20,6 +22,7 @@ let levels = [
       "David Fincher",
       "Wes Anderson"
     ],
+    answer: "Alejandro G. I&ntilde;&aacute;rritu",
     answerIndex: 0,
     congratImageUrl:
       "https://pmcvariety.files.wordpress.com/2019/02/alejandro-g.-inarritu.jpg?w=1000"
@@ -27,6 +30,7 @@ let levels = [
   {
     question: "What was the first feature-length computer-animated movie?",
     possibleAnswers: ["Tron", "Toy Story", "Lion king", "101 Dalmatians"],
+    answer: "Toy Story",
     answerIndex: 1,
     congratImageUrl:
       "https://media.giphy.com/media/wcFlXfhyyyp0c/giphy.gifww.baidu.com"
@@ -35,6 +39,7 @@ let levels = [
     question:
       "Which of the following car manufacturers had a war named after it?",
     possibleAnswers: ["Toyota", "Honda", "Greatwall", "Volkswagen"],
+    answer: "Toyota",
     answerIndex: 0,
     congratImageUrl: "https://media.giphy.com/media/NdpQJAQuBFQJ2/giphy.gif"
   }
@@ -42,8 +47,20 @@ let levels = [
 
 var currentLevelIndex = 0;
 var currentLevel = levels[currentLevelIndex];
+var currentlevelAnswer = currentLevel.answer;
+var currentLevelCongratImageUrl = currentLevel.congratImageUrl;
 
-function checkAnswer(userSelection) {}
+function checkAnswer(userSelection) {
+  if (userSelection == currentlevelAnswer) {
+    showRight(currentLevel);
+  } else {
+    showWrong(currentLevel);
+  }
+}
+
+function showRight(thisLevel) {
+  $("#showUserCheckResult").text("WELL DONE MATE!");
+}
 
 function showGame(game) {
   $("#question").text(game.question);
@@ -51,7 +68,7 @@ function showGame(game) {
     var possibleAnswersButton = $("<button>");
     $(possibleAnswersButton).text(answer);
     $(possibleAnswersButton).click(function() {
-      checkAnswer();
+      checkAnswer(answer);
     });
   }
   $("#answers").text(game.possibleAnswers);
